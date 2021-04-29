f17830306f6da3abffac5e3965098b542b53dbb6
tfbzg <83396307+tfbzg@users.noreply.github.com>
2021-04-29 17:34:32 +0900

    Create average.c

---
+++ average.c
@@ -0,0 +1,30 @@
+/*
+ * C言語のサンプルプログラム - Webkaru
+ * - 配列の数値の平均値を計算 -
+ */
+#include <stdio.h>
+int main(void) {
+ 
+  /* 変数の定義 */
+  int i, n;
+ 
+  float num[100];
+  float sum, ave;
+ 
+  printf("数値の数を入力(1〜100) = ");
+  scanf("%d",&n);
+ 
+  /* 数値の入力と総和の計算 */
+  sum = 0.0;
+  for( i=0;i<n;++i ) {
+    printf("%d個目 = ",i+1);
+    scanf("%f",&num[i]);
+    sum += num[i];
+  }
+ 
+  /* 平均値の計算と出力 */
+  ave = sum/n;
+  printf("数値の平均値 = %.2f\n", ave);
+ 
+  return 0;
+}
